1371e39011ce8cd61e4d56fd075d6c90004d6013
Zsolt Lengyel <zsolt.lengyel21@gmail.com>
2021-03-24 21:23:48 +0100

    03_24_2021_21_23_48 fast commit

--- buildings/app.js
+++ buildings/app.js
@@ -11,3 +11,12 @@ document.querySelector('.menu-button').addEventListener('click', function() {
 } );
 
 
+document.querySelector('.close-menu').addEventListener('click', function() {
+  console.log('you closed the menu');
+  // css class-t kapnak bizonyos elemek...
+  document.querySelector('body').classList.remove('js-menu-open');
+  document.querySelector('.menu-button').classList.remove('js-hidden');
+  document.querySelector('header .main-menu').classList.remove('js-show');
+  document.querySelector('.close-menu').classList.add('js-hidden');
+
+} );
