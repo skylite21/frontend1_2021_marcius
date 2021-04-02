fcacf7677f223b2ff6a146995d114adb8503143e
Zsolt Lengyel <zsolt.lengyel21@gmail.com>
2021-04-02 20:32:47 +0200

    04_02_2021_20_32_47 fast commit

--- game/app.js
+++ game/app.js
@@ -1,23 +1,42 @@
-// első játékos kezd mindíg
-// 0: első játékos,
-// 1: második játékos
-let activePlayer = 0;
-// adot kör alatt megszerzett pont:
-let roundScore = 0;
+let activePlayer, roundScore, scores;
 
-// array: több változót tárol egy darab típusban
-// a scores tömb nulaldik eleme az első játékos pontszámait jelenti
-// a második elem pedig a második játékos pontszámait
-let scores = [0,0];
+function init() {
+  // első játékos kezd mindíg
+  // 0: első játékos,
+  // 1: második játékos
+  activePlayer = 0;
+  // adot kör alatt megszerzett pont:
+  roundScore = 0;
 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
+  // array: több változót tárol egy darab típusban
+  // a scores tömb nulaldik eleme az első játékos pontszámait jelenti
+  // a második elem pedig a második játékos pontszámait
+  scores = [0,0];
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+  document.querySelector('#score-0').textContent = 0;
+  document.querySelector('#score-1').textContent = 0;
+  document.querySelector('#current-0').textContent = 0;
+  document.querySelector('#current-1').textContent = 0;
 
-document.querySelector('.dice').style.display = 'none';
+  document.querySelector('.dice').style.display = 'none';
+  document.querySelector('.btn-roll').style.display = 'block';
+  document.querySelector('.btn-hold').style.display = 'block';
 
+  document.querySelector('#name-0').textContent = 'Player 1';
+  document.querySelector('#name-1').textContent = 'Player 2';
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+
+  // első játékos kezd:
+  document.querySelector('.player-0-panel').classList.add('active');
+
+}
+
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   
@@ -33,9 +52,58 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
   // 3. Ha a dobott érték nem 1
-  //  akkor a dobott értéket adjuk hozzá a roundScore változóhoz
-  //  Az eredményt jelenítsük meg a képernyőn
-  //
-  //  Ha a játékos 1-est dobott, akkor a kövkező játékos jön.
+  if (dice !== 1) {
+    //  akkor a dobott értéket adjuk hozzá a roundScore változóhoz
+    roundScore = roundScore + dice;
+    //  Az eredményt jelenítsük meg a képernyőn
+    document.querySelector('#current-'+activePlayer).textContent = roundScore;
+  } else {
+    //  Ha a játékos 1-est dobott, akkor a kövkező játékos jön.
+    nextPlayer();
+  }
 
 });
+
+// DRY; do not repeat yourself
+function nextPlayer() {
+  if(activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  roundScore = 0;
+  // nullázzuk a current táblákat
+  document.querySelector('#current-0').textContent = 0;
+  document.querySelector('#current-1').textContent = 0;
+
+  // toggle> ha rajta volt akkor leveszi, ha nem volt rajta akkor rárakja
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+}
+
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  // 1. a játékos megszerzi a kör alatt szerzett pontjait
+  scores[activePlayer] = scores[activePlayer] + roundScore;
+  // 2. az eredményt frissítjük a UI-on 
+  document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
+
+  // 3. nézzük meg hogy van e nyertes
+  if (scores[activePlayer] >= 20) {
+    document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+    document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+
+    document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.btn-hold').style.display = 'none';
+    document.querySelector('.btn-roll').style.display = 'none';
+  } else {
+    // következő játékos jön!...
+    nextPlayer();
+
+  }
+
+});
+
+document.querySelector('.btn-new').addEventListener('click', init);
